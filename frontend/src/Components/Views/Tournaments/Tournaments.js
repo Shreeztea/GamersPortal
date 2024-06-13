403442c93996e6e3b5f852db141d7d1540970457
Shreeztea <katestha@hotmail.com>
2024-06-13 22:17:03 +0545

    Created Tounament Component

--- frontend/src/Components/Views/Tournaments/Tournaments.js
+++ frontend/src/Components/Views/Tournaments/Tournaments.js
@@ -1,133 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import Card from '../../Common/Card/Card';
+import Carousel from '../../Common/Carousel/Carousel';
+import CarouselItem from '../../Common/Carousel/CarouselItem';
+import CarouselBannerImage from '../../Common/Carousel/CarouselBannerImage';
 const Tournaments = () => {
   return <>
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner w-full" >
-        <div class="carousel-item active flex flex-wrap">
-          <div class="flex flex-wrap justify-between px-4">
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://pixlr.com/images/index/product-image-one.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://pixlr.com/images/index/product-image-one.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://pixlr.com/images/index/product-image-one.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://pixlr.com/images/index/product-image-one.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            {/* <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" class="d-block w-100" alt="..."/> */}
-          </div>
-        </div>
-        <div class="carousel-item">
-        <div class="flex flex-wrap justify-between px-4">
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            {/* <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" class="d-block w-100" alt="..."/> */}
-          </div>
-          {/* <img src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" class="d-block w-100" alt="..." /> */}
-        </div>
-        <div class="carousel-item">
-        <div class="flex flex-wrap justify-between px-4">
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            <div class="card" style={{ width: '18rem', fontSize: '18px' }}>
-              <img src="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            {/* <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" class="d-block w-100" alt="..."/> */}
-          </div>
-          {/* <img src="https://t4.ftcdn.net/jpg/03/10/24/63/360_F_310246341_869grfwR1b87MN3qyFPe6yZZIRC83X31.jpg" class="d-block w-100" alt="..." /> */}
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+    <Carousel>
+      <CarouselItem active="active">
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://pixlr.com/images/index/product-image-one.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://pixlr.com/images/index/product-image-one.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://pixlr.com/images/index/product-image-one.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://pixlr.com/images/index/product-image-one.webp" link="fb.com" />
+      </CarouselItem>
+      <CarouselItem>
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" link="fb.com" />
+      </CarouselItem>
+      <CarouselItem>
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" link="fb.com" />
+        <Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." image="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" link="fb.com" />
+      </CarouselItem>
+    </Carousel>
   </>
 };
 
